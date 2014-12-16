93b9c815bc0688ea3fb8e31bcb366b7eb18d4fbf
voodoo <voodoo@securityvoodoo.net>
2014-12-15 22:26:22 -0800

    replace instance variables with global variables

--- re.rb
+++ re.rb
@@ -11,18 +11,18 @@
 require "rubygems"
 require "highline/import"
 
-@R_SSN = /^(?!666|000)\d{3}[-\s]?(?!00)\d{2}[-\s]?(?!0000)\d{4}$/
-@R_PHN = /^\(?(?!000)\d{3}\)?[-\s]?(?!000)\d{3}[-\s]?\d{4}$/
-@R_EML = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+[\.a-zA-Z0-9]*\.\w{2,}$/
-@R_NME = /^(?![0-9])\w{2,}[,\s]+(?![0-9])\w{2,}[,\s]+(?![0-9])\w{1,}$/
-@R_DTE = /^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d$/
-@R_HAD = /^\d{1,6}\s\w{1,6}\s[a-zA-Z]{1,20}\s[a-zA-Z]{2,15}$/
-@R_CSZ = /^\w{2,15},\s\w{2},\s\d{5}(-\d{5})?$/
-@R_MIL = /^([01]\d|2[0-3]):?([0-5]\d):?([0-5]\d)$/ 
-@R_CUR = /^\$[0-9]{1,3}(,?[0-9]{3})*(\.[0-9]{2})?$/
-@R_URL = /^((http|https|ftp|ftps):\/\/)?(www\.)?[a-zA-Z0-9]+[\.a-zA-Z0-9]*\.\w{2,}$/ 
-@R_PWD = /^(?=.*\d)(?=.*[_\W])(?!.*[a-z]{3}).+$/
-@R_ODD = /^([a-zA-Z]{2})*ion$/
+$R_SSN = /^(?!666|000)\d{3}[-\s]?(?!00)\d{2}[-\s]?(?!0000)\d{4}$/
+$R_PHN = /^\(?(?!000)\d{3}\)?[-\s]?(?!000)\d{3}[-\s]?\d{4}$/
+$R_EML = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+[\.a-zA-Z0-9]*\.\w{2,}$/
+$R_NME = /^(?![0-9])\w{2,}[,\s]+(?![0-9])\w{2,}[,\s]+(?![0-9])\w{1,}$/
+$R_DTE = /^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d$/
+$R_HAD = /^\d{1,6}\s\w{1,6}\s[a-zA-Z]{1,20}\s[a-zA-Z]{2,15}$/
+$R_CSZ = /^\w{2,15},\s\w{2},\s\d{5}(-\d{5})?$/
+$R_MIL = /^([01]\d|2[0-3]):?([0-5]\d):?([0-5]\d)$/ 
+$R_CUR = /^\$[0-9]{1,3}(,?[0-9]{3})*(\.[0-9]{2})?$/
+$R_URL = /^((http|https|ftp|ftps):\/\/)?(www\.)?[a-zA-Z0-9]+[\.a-zA-Z0-9]*\.\w{2,}$/ 
+$R_PWD = /^(?=.*\d)(?=.*[_\W])(?!.*[a-z]{3}).+$/
+$R_ODD = /^([a-zA-Z]{2})*ion$/
 
 ########
 # regular_expression(toscrub,flag)
@@ -36,29 +36,29 @@ def regular_expression(text,flag)
 
 	case flag
 	when "R_SSN"
-		_match = true unless !( text =~ @R_SSN )
+		_match = true unless !( text =~ $R_SSN )
 	when "R_PHN"
-		_match = true unless !( text =~ @R_PHN )
+		_match = true unless !( text =~ $R_PHN )
 	when "R_EML"
-		_match = true unless !( text =~ @R_EML )
+		_match = true unless !( text =~ $R_EML )
 	when "R_NME"
-		_match = true unless !( text =~ @R_NME )
+		_match = true unless !( text =~ $R_NME )
 	when "R_DTE"
-		_match = true unless !( text =~ @R_DTE )
+		_match = true unless !( text =~ $R_DTE )
 	when "R_HAD"
-		_match = true unless !( text =~ @R_HAD )
+		_match = true unless !( text =~ $R_HAD )
 	when "R_CSZ"
-		_match = true unless !( text =~ @R_CSZ )
+		_match = true unless !( text =~ $R_CSZ )
 	when "R_MIL"
-		_match = true unless !( text =~ @R_MIL )
+		_match = true unless !( text =~ $R_MIL )
 	when "R_CUR"
-		_match = true unless !( text =~ @R_CUR )
+		_match = true unless !( text =~ $R_CUR )
 	when "R_URL"
-		_match = true unless !( text =~ @R_URL )
+		_match = true unless !( text =~ $R_URL )
 	when "R_PWD"
-		_match = true unless !( text =~ @R_PWD )
+		_match = true unless !( text =~ $R_PWD )
 	when "R_ODD"
-		_match = true unless !( text =~ @R_ODD )
+		_match = true unless !( text =~ $R_ODD )
 	end
 
 	return _match 
